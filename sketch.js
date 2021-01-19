7774041dc2489c134310c5bc243534723001d92d
JaiBhayani <76654034+JaiBhayani@users.noreply.github.com>
2021-01-19 20:19:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,43 @@
+
 var monkey, monkey_running, monkey_collided;
 var ground, invisibleGround, groundImage, background;
 
 var rockImage, banImage;
-var obstaclesGroup, banana, rock;
+var obstaclesGroup, banana, rock
 var score;
 
-var PLAY=1
-var END=0;
-var gameState=PLAY;
 
 function preload(){
-  monkey_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
+  monkey_running = loadAnimation("Monkey_01.png","Monkey_2.png","Monkey_3.png","Monkey_4.png","Monkey_5.png","Monkey_6.png","Monkey_7.png","Monkey_8.png","Monkey_9.png","Monkey_10.png");
+  monkey_collided = loadImage("Monkey_01.png");
   
-  monkey_collided = loadAnimation("Monkey_01.png");
+  background = loadImage ("jungle.jpg");
   
-  backgroundimg = loadImage ("jungle.jpg");
+  banImage = loadImage("cloud.png");
   
-  banImage = loadImage("banana.png");
-  rockImage=loadImage("stone.png");
-  //obstacle1 = loadImage("obstacle1.png");
- 
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
 }
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(600, 200);
   
-   background = createSprite(200,180,400,20);
- background.addImage("back",backgroundimg);
+  monkey = createSprite(50,180,20,50);
+  monkey.addAnimation("running", monkey_running);
+  monkey.addAnimation("collided", monkey_collided);
+  monkey.scale = 0.5;
+  
+  bockground = createSprite(200,180,400,20);
+ background.addImage("back",background);
   background.x = background.width /2;
   background.velocityX = -4;
   
-  monkey = createSprite(50,350,20,50);
-  monkey.addAnimation("running", monkey_running);
-  monkey.addAnimation("collided", monkey_collided);
-  monkey.scale = 0.1;
-  
- 
-  
-  invisibleGround = createSprite(200,390,400,10);
-  //invisibleGround.visible = false;
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
   
   
   banGroup = new Group();
@@ -46,29 +45,25 @@ function setup() {
 }
 
 function draw() {
-  //background(180);
- if(gameState===PLAY){
-   background.velocityX=-2;
-if(keyDown("space")) {
+  background(180);
+ 
+if(keyDown("space")&&monkey.y>100) {
     monkey.velocityY = -10;
   }
   
-  monkey.velocityY = monkey.velocityY + 0.8;
-   
+  monkey.velocityY = monkey.velocityY + 0.8
   
-  if (background.x < 0){
-    backgroundground.x = background.width/2;
-  }
+  /*if (ground.x < 0){
+    ground.x = ground.width/2;
+  }*/
   
   monkey.collide(invisibleGround);
-   
   spawnBananas();
   spawnRocks();
-   
     if(monkey.isTouching(rockGroup)){
       gameState  = END;
     }
- }
+  
  if (gameState === END){
      background.velocityX = 0;
     monkey.velocityY = 0;
@@ -76,18 +71,19 @@ if(keyDown("space")) {
     rockGroup.setVelocityXEach  (0);
     monkey.changeAnimation("collided",monkey_collided);
     banGroup.setLifetimeEach(-1);
-    rockGroup.setLifetimeEach(-1);
+    rockcloudsGroup.setLifetimeEach(-1);
    }
+     text("Score: "+ score, 500,50);
   drawSprites();
-  text("Score: "+ score, 500,50);
+  
 }
 
 function spawnBananas() {
   if (frameCount % 100 === 0) {
     var banana = createSprite(600,120,40,10);
     banana.y = Math.round(random(80,120));
-    banana.addImage(banImage);
-   banana.scale = 0.1;
+    banana.addImage(cloudImage);
+   banana.scale = 0.5;
     banana.velocityX = -3;
    banana.lifetime = 200;
     
@@ -100,11 +96,9 @@ function spawnBananas() {
 
 function spawnRocks() {
   if(frameCount % 60 === 0) {
-    rock = createSprite(600,365,10,40);
-    rock.addImage(rockImage);
+    var rock = createSprite(600,165,10,40);
     rock.velocityX = -4;           
-    rock.scale = 0.1;
+    rock.scale = 0.5;
     rock.lifetime = 300;
-    rockGroup.add(rock);
   }
 }
